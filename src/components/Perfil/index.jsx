92b20c7d906230b6dab9b21e8f3be6fb476a4f44
LUCAS SOUZA DUARTE <lucas.sduarte@a.ucb.br>
2023-10-26 20:24:33 -0300

    botão de voltar

--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -1,14 +1,19 @@
-import styles from './Perfil.module.css'
+import styles from './Perfil.module.css';
 
 const Perfil = ({ nomeUsuario }) => {
+    function reload() {
+        window.location.reload();
+    }
+
     return (
         <header className={styles.header}>
-            <img className={styles.avatar} src={`https://github.com/${nomeUsuario}.png`} />
+            <button className={styles.btn} onClick={reload}>Voltar</button>
+            <img className={styles.avatar} src={`https://github.com/${nomeUsuario}.png`} alt="Avatar" />
             <h1 className={styles.name}>
                 {nomeUsuario}
             </h1>
         </header>
-    )
-}
+    );
+};
 
 export default Perfil;
